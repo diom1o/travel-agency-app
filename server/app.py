eb0bc7b95d54df27a2444f5a22e0a10fb78b252c
diom1o <161154194+diom1o@users.noreply.github.com>
2024-07-12 12:16:55 +0200

    update variables

--- server/app.py
+++ server/app.py
@@ -16,52 +16,55 @@ class Tour(db.Model):
     price = db.Column(db.Float)
 
 class Booking(db.Model):
-    id = db.Column(db.Integer, primary_visits=True)
+    id = db.Column(db.Integer, primary_key=True)  
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     tour_id = db.Column(db.Integer, db.ForeignKey('tour.id'))
     date = db.Column(db.Date)
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primaryKey=True)  
     username = db.Column(db.String(50), unique=True)
     email = db.Column(db.String(100), unique=True)
 
 @app.route('/tours', methods=['POST', 'GET'])
-def manage_tours():
+def handle_tours():
     if request.method == 'POST':
-        data = request.json
-        new_tour = Tour(name=data['name'], description=data['description'], price=data['price'])
+        tour_info = request.json
+        new_tour = Tour(name=tour_info['name'], description=tour_info['description'], price=tour_info['price'])
         db.session.add(new_tour)
         db.session.commit()
         return jsonify({'message': 'Tour added successfully'}), 201
     elif request.method == 'GET':
-        tours = Tour.query.all()
-        return jsonify([{'id': tour.id, 'name': tour.name, 'description': tour.description, 'price': tour.price} for tour in tours])
+        all_tours = Tour.query.all()
+        tours_data = [{'id': tour.id, 'name': tour.name, 'description': tour.description, 'price': tour.price} for tour in all_tours]
+        return jsonify(tours_data)
 
 @app.route('/bookings', methods=['POST', 'GET'])
-def manage_bookings():
+def handle_bookings():
     if request.method == 'POST':
-        data = request.json
-        new_booking = Booking(user_id=data['user_id'], tour_id=data['tour_id'], date=data['date'])
+        booking_info = request.json
+        new_booking = Booking(user_id=booking_info['user_id'], tour_id=booking_info['tour_id'], date=booking_info['date'])
         db.session.add(new_booking)
         db.session.commit()
         return jsonify({'message': 'Booking added successfully'}), 201
     elif request.method == 'GET':
-        bookings = Booking.query.all()
-        return jsonify([{'id': booking.id, 'user_id': booking.user_id, 'tour_id': booking.tour_id, 'date': booking.date} for booking in bookings])
+        all_bookings = Booking.query.all()
+        bookings_data = [{'id': booking.id, 'user_id': booking.user_id, 'tour_id': booking.tour_id, 'date': booking.date} for booking in all_bookings]
+        return jsonify(bookings_data)
 
 @app.route('/users', methods=['POST', 'GET'])
-def manage_users():
+def handle_users():
     if request.method == 'POST':
-        data = request.json
-        new_user = User(username=data['username'], email=data['email'])
+        user_info = request.json
+        new_user = User(username=user_info['username'], email=user_info['email'])
         db.session.add(new_user)
         db.session.commit()
         return jsonify({'message': 'User added successfully'}), 201
     elif request.method == 'GET':
-        users = User.query.all()
-        return jsonify([{'id': user.id, 'username': user.username, 'email': user.email} for user in users])
+        all_users = User.query.all()
+        users_data = [{'id': user.id, 'username': user.username, 'email': user.email} for user in all_users]
+        return jsonify(users_data)
 
 if __name__ == '__main__':
-    db.createTCHA(app)
+    db.create_all()
     app.run(debug=True)
